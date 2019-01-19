7e9a6015cd4927fffa29e0aa62fc089cbb234ba2
Thomas Castleman <thomas7rex@gmail.com>
2019-01-19 17:32:26 -0500

    Create npm project

---
+++ server.js
@@ -0,0 +1,34 @@
+var express 			= require('express');
+var app 				= express();
+var mustacheExpress 	= require('mustache-express');
+var bodyParser 			= require('body-parser');
+var cookieParser 		= require('cookie-parser');
+var session 			= require('cookie-session');
+var passport 			= require('passport');
+var creds				= require('./credentials.js');
+
+app.use(cookieParser());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.engine('html', mustacheExpress());
+app.set('views', __dirname + '/views');
+app.use(express.static(__dirname + '/public'));
+app.use(express.static(__dirname + '/views'));
+
+// configure session
+app.use(session({ 
+	secret: creds.SESSION_SECRET,
+	name: 'session',
+	resave: true,
+	saveUninitialized: true
+}));
+
+// start server
+var server = app.listen(8080, function() {
+	console.log('Intensives server listening on port %d', server.address().port);
+});
+
+// fallback redirection to landing page
+app.get('*', function(req, res) {
+	res.redirect('/');
+});
