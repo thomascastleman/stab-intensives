c7a454c4c3ba29ef74c96eb3fc24bf2239f7cf21
johnnylindbergh <johnnyl8@embarqmail.com>
2019-01-20 00:13:25 -0500

    delete get request

--- admin.js
+++ admin.js
@@ -6,5 +6,20 @@ module.exports = {
 	// set up admin routes
 	init: function(app) {
 
+		app.get('/wipeAllData', function(req, res){
+			con.query("DELETE FROM matching;", function(suc){
+				con.query("DELETE FROM preferences;", function(succ){
+					con.query("DELETE FROM students;", function(succc){
+						con.query("DELETE FROM intensives;", function(succcc){
+							res.render("error.html", {message: "failure to delete somthing :( "});
+						
+						});			
+					});	
+				});
+			});
+
+		});
+
 	}
 }
+
